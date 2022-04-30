f6397a4f199a8457c93ff4f63e0078f9ca494b2c
adritadeb <adritad2019@gmail.com>
2022-04-30 16:54:32 +0600

    mongodb connected and environment file added

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fta2v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+    console.log('mongodb connected')
+});
 
 app.get('/', (req, res) => {
     res.send('Warehouse server is running');
