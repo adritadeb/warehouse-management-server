710e7bad2e22aa28cecd344f6810d66a8641d855
adritadeb <adritad2019@gmail.com>
2022-05-01 18:56:10 +0600

    books API created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -8,19 +9,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fta2v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zlzuo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-    console.log('mongodb connected')
-});
+
+async function run() {
+    await client.connect();
+    const booksCollection = client.db('booksStocker').collection('books');
+
+    app.get('/books', async (req, res) => {
+        const query = {};
+        const cursor = booksCollection.find(query);
+        const books = await cursor.toArray();
+        res.send(books);
+    })
+};
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
-    res.send('Warehouse server is running');
+    res.send('Warehouse server is running')
 });
 
 app.listen(port, () => {
     console.log('Listening to port', port);
-})
+});
